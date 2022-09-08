d501af88bac9959a96b3285cc97556abc5eb0dec
ehsan <1374ea@gmail.com>
2022-09-08 19:27:40 +0430

    Each time return a random instance for the get_one_test_data method

--- src/data_loaders/default_data_loader.py
+++ src/data_loaders/default_data_loader.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -8,47 +10,46 @@ from framework.utils.path import data_path
 
 
 class DataLoader(IDataLoader):
+    csv_x_fields = [
+        'pregnant_times',
+        'plasma_glucose',
+        'blood_pressure',
+        'skinfold_thickness',
+        'serum_insulin',
+        'body_mass_index',
+        'pedigree_function',
+        'age',
+    ]
+    csv_y_field = 'class_variable'
+    csv_all_fields = [*csv_x_fields, csv_y_field]
+
     def __init__(self):
-        x, y = self._load_x_and_y_from_csv()
+        x, y = self._get_x_and_y_dataframes()
         x_train, x_test, y_train, y_test = train_test_split(
             x, y, test_size=c['test_size'], random_state=0, shuffle=False
         )
 
-        self.x_train = self._standard_scale(x_train)
-        self.y_train = y_train
+        self.x_train = self._transform(x_train)
+        self.y_train = y_train.to_numpy()
 
-        self.x_test = self._standard_scale(x_test)
-        self.y_test = y_test
+        self.x_test = self._transform(x_test)
+        self.y_test = y_test.to_numpy()
 
-    @staticmethod
-    def _load_x_and_y_from_csv():
-        dataframe = pd.read_csv(
-            data_path('pima-indians-diabetes.csv'),
-            names=[
-                'pregnant_times',
-                'plasma_glucose',
-                'blood_pressure',
-                'skinfold_thickness',
-                'serum_insulin',
-                'body_mass_index',
-                'pedigree_function',
-                'age',
-                'class_variable',
-            ],
-        )
+    def _get_x_and_y_dataframes(self):
+        dataframe = pd.read_csv(data_path('pima-indians-diabetes.csv'), names=self.csv_all_fields)
         dataframe.drop_duplicates(inplace=True)
 
-        x = dataframe.iloc[:, :-1].values
-        y = dataframe.iloc[:, -1].values
+        x = dataframe[self.csv_x_fields]
+        y = dataframe[self.csv_y_field]
 
         return x, y
 
     @staticmethod
-    def _standard_scale(data_set):
+    def _transform(dataframe):
         scaler = StandardScaler()
-        data_set = scaler.fit_transform(data_set)
+        result = scaler.fit_transform(dataframe)
 
-        return data_set
+        return result
 
     def get_train_data(self):
         return self.x_train, self.y_train
@@ -60,4 +61,6 @@ class DataLoader(IDataLoader):
         return self.x_test, self.y_test
 
     def get_one_test_data(self):
-        return self.x_test[:1], self.y_test[:1]  # return 1 instance but keep it's shape
+        index = random.randint(1, len(self.x_test))
+
+        return self.x_test[index - 1 : index], self.y_test[index - 1 : index]  # return 1 instance but keep it's shape
